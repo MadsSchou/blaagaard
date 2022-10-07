fce53dda5eef9c0feaebea3feb02281e9d6fde7d
Mads <mschouolesen@gmail.com>
2022-10-07 11:39:34 +0200

    burger js

---
+++ script.js
@@ -0,0 +1,16 @@
+// Burgermenu
+const btn = document.querySelector(".toggle-btn");
+const menu = document.querySelector(".main-menu");
+
+function toggleMenu() {
+  menu.classList.toggle("shown");
+  const menuShown = menu.classList.contains("shown");
+  if (menuShown) {
+    console.log(menuShown);
+    btn.textContent = "Luk";
+  } else {
+    console.log(menuShown);
+    btn.textContent = "Menu";
+  }
+}
+btn.addEventListener("click", toggleMenu);
